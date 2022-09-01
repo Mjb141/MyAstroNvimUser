83211f0474f8dba13f3fe4ec6f8a93636ce804ca
Michael Brown <michaelbrown@ip-192-168-1-93.eu-west-1.compute.internal>
2022-09-01 09:23:59 +0100

    Added transparent window background for warp terminal

--- init.lua
+++ init.lua
@@ -11,6 +11,19 @@ local config = {
    -- }
  },
  colorscheme = "cinnabar",
+ default_theme = {
+   highlights = function(hi)
+     local C = require "default_theme.colors"
+     hi.Normal = {bg = C.none, ctermbg = C.none}
+     hi.CursorColumn = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
+     hi.CursorLine = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
+     hi.CursorLineNr = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
+     hi.LineNr = {}
+     hi.SignColumn = {}
+     hi.StatusLine = {}
+     return hi
+  end,
+ },
  mappings = {
    n = {
      ["<leader>lnn"] = { function() vim.diagnostic.goto_next() end, desc = "Next diagnostic" }, 
