6fd2855ac99e7a92174d5e572f54c9347ec3ba2e
Michael Brown <michaelbrown@ip-192-168-1-93.eu-west-1.compute.internal>
2022-10-11 15:48:46 +0100

    added neo-tree transparency

--- init.lua
+++ init.lua
@@ -1,35 +1,31 @@
 local config = {
- plugins = {
-   init = {
-     { "ggandor/lightspeed.nvim" },
-     { "vimoxide/vim-cinnabar"},
-   },
-   -- bufferline = {
-   --   options = {
-   --     mode = "tabs"
-   --   }
-   -- }
- },
- -- colorscheme = "cinnabar",
- default_theme = {
-   highlights = function(hi)
-     local C = require "default_theme.colors"
-     hi.Normal = {bg = C.none, ctermbg = C.none}
-     hi.CursorColumn = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-     hi.CursorLine = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-     hi.CursorLineNr = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-     hi.LineNr = {}
-     hi.SignColumn = {}
-     hi.StatusLine = {}
-     return hi
-  end,
- },
- mappings = {
-   n = {
-     ["<leader>lnn"] = { function() vim.diagnostic.goto_next() end, desc = "Next diagnostic" }, 
-     ["<leader>lnp"] = { function() vim.diagnostic.goto_prev() end, desc = "Previous diagnostic" }
-   }
- } 
+    plugins = {
+        init = {
+            { "ggandor/lightspeed.nvim" },
+            { "vimoxide/vim-cinnabar" },
+        },
+    },
+    default_theme = {
+        highlights = function(hi)
+            local C = require "default_theme.colors"
+            hi.Normal = { bg = C.none, ctermbg = C.none }
+            hi.CursorColumn = { cterm = {}, ctermbg = C.none, ctermfg = C.none }
+            hi.CursorLine = { cterm = {}, ctermbg = C.none, ctermfg = C.none }
+            hi.CursorLineNr = { cterm = {}, ctermbg = C.none, ctermfg = C.none }
+            hi.LineNr = {}
+            hi.SignColumn = {}
+            hi.StatusLine = {}
+            hi.NeoTreeNormal = { bg = C.none, ctermbg = C.none }
+            hi.NeoTreeNormalNC = { bg = C.none, ctermbg = C.none }
+            return hi
+        end,
+    },
+    mappings = {
+        n = {
+            ["<leader>lnn"] = { function() vim.diagnostic.goto_next() end, desc = "Next diagnostic" },
+            ["<leader>lnp"] = { function() vim.diagnostic.goto_prev() end, desc = "Previous diagnostic" }
+        }
+    }
 }
 
 return config
